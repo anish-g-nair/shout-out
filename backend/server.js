85f4675246d2dc805da04ba1e7fdcf4c5ee24658
Anish Nair <anish.nair02@gmail.com>
2025-01-28 22:42:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const cors = require('cors');
 const messageRoutes = require('./routes/messages'); // Import the updated routes
 
