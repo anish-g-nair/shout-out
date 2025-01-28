bb5fe2c6b5289668f7d6a7c2c72b3cd735fa8acb
Anish Nair <anish.nair02@gmail.com>
2025-01-28 22:42:50 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const mongoUri = process.env.MONGODB_URI;
+const mongoUri = process.env.MONGO_URI;
 
 if (!mongoUri) {
   throw new Error('MONGODB_URI is not defined in environment variables');
