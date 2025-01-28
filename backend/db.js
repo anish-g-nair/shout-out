7bdfd50ccc10c35e7962753ab3a657890cba8d29
Anish Nair <anish.nair02@gmail.com>
2025-01-28 23:10:37 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -6,33 +6,30 @@ if (!mongoUri) {
   throw new Error('MONGO_URI is not defined in environment variables');
 }
 
-// Track the connection status
 let isConnected;
 
 const connectToDatabase = async () => {
-  if (isConnected) {
-    // Use the existing connection
-    return mongoose.connection;
-  }
-
-  if (mongoose.connection.readyState) {
-    // If Mongoose already has an active connection, use it
-    isConnected = true;
-    return mongoose.connection;
-  }
-
-  // If no connection exists, create one
   try {
+    if (isConnected) {
+      return mongoose.connection;
+    }
+
+    if (mongoose.connection.readyState) {
+      isConnected = true;
+      return mongoose.connection;
+    }
+
     const db = await mongoose.connect(mongoUri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
     isConnected = true;
     console.log('Connected to MongoDB');
     return db;
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
-    throw error;
+    throw new Error('Could not connect to database');
   }
 };
 
