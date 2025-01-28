f9d1f65a60ad49a73d8e9119c32b061e36bf38c2
Anish Nair <anish.nair02@gmail.com>
2025-01-28 22:45:10 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,22 +1,22 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
-const mongoUri = process.env.MONGO_URI;
+const mongoUri = process.env.MONGODB_URI;
 
 if (!mongoUri) {
   throw new Error('MONGODB_URI is not defined in environment variables');
 }
 
 const connectToDatabase = async () => {
-  if (mongoose.connections[0]?.readyState) {
+  if (mongoose.connection.readyState) {
     // Already connected
-    return mongoose.connections[0];
+    return mongoose.connection;
   }
 
-  // Create new connection
+  // Create a new connection
   return mongoose.connect(mongoUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
 };
 
-export default connectToDatabase;
+module.exports = connectToDatabase;
