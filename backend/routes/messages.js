33a3889bc1af7b4713af330c2bc817dec172aa83
Anish Nair <anish.nair02@gmail.com>
2025-02-17 01:28:08 +0530

    Update messages.js

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -5,7 +5,8 @@ const router = express.Router();
 
 const mongoose = require('mongoose');
 // const mongoUri = 'mongodb+srv://devputler:JAC0ejgvHY3EGNJw@default.7oahe.mongodb.net/default?retryWrites=true&w=majority&appName=Default' || process.env.MONGO_URI;
-const mongoUri = process.env.MONGODB_URI;
+// const mongoUri = process.env.MONGODB_URI;
+const mongoUri = 'mongodb+srv://putleracademy:cA7UhtiSQlAbTaxh@globalboard.gs13g.mongodb.net/?retryWrites=true&w=majority&appName=globalboard'
 
 if (!mongoUri) {
   throw new Error('MONGO_URI is not defined in environment variables');
